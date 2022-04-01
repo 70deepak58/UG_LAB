fb6e2ffb2f1beb567dba0dc94da01ccd9a98881f
70deepak58 <54128728+70deepak58@users.noreply.github.com>
2022-04-01 09:14:36 +0530

    Add files via upload

---
+++ OpenGL6/cirlce_clock.cpp
@@ -0,0 +1,44 @@
+#include<math.h>
+#include<GL/glut.h>  
+int i=0;
+float a=0;
+
+void init(void)
+{
+	glClearColor(1.0, 1.0, 1.0, 0.0);        // set display-window color to white
+	glMatrixMode(GL_PROJECTION);             // set projection parameters
+	gluOrtho2D(0.0, 200.0, 0.0, 150.0);
+}
+
+void lineSegment(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT);            // clear display window
+
+	for(i=0;i<=360;){
+		a=i*3.14/180;
+			glColor3f(0.0, 0.4, 0.2);                // set line segment color to green
+			glBegin(GL_LINES);                       // specify line-segment geometry
+	glVertex2f(50, 100);
+		glVertex2f(50+20*cos(a), 100+20*sin(a));
+	glEnd();
+
+		i+=10;
+	}
+		
+
+		glFlush();                               // process all OpenGL routines as quickly as possible	
+	
+}
+
+int main(int argc, char ** argv)
+{
+	glutInit(&argc, argv);                        // initialize GLUT
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);  // set display mode
+	glutInitWindowPosition(50, 100);              // set top-left display window position
+	glutInitWindowSize(800, 600);                 // set display window width and height
+	glutCreateWindow("A Line");                    // creat display window
+	
+	init();                                       // execute initialization procedure
+	glutDisplayFunc(lineSegment);                 //Send graphics to display window
+	glutMainLoop();                               // display everything and wait
+}
